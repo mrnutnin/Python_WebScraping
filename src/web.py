121006d24ce7e62c2c358a9cb9f4b0755ef6795c
porinut <porinut1@gmail.com>
2020-02-24 23:01:58 +0700

    Huay Success

--- src/web.py
+++ src/web.py
@@ -6,8 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from openpyxl import load_workbook
 import os 
-import time
 import ConfigParser
+import datetime
+
 
 dirpath = os.getcwd()
 chromePath = dirpath+'\chromedriver\chromedriver.exe'
@@ -18,18 +19,22 @@ config = ConfigParser.RawConfigParser()
 config.read(dirpath+'\ConfigFile.properties')
 
 def huayClub():
-
-    huayClub_url = config.get('HuayClubSection', 'huayClub_url')
-    huayClub_user = config.get('HuayClubSection', 'huayClub_user')
-    huayClub_pass = config.get('HuayClubSection', 'huayClub_pass')
-    try: 
+    print '=============================='
+    print '>>> HuayClub <<<<'
+    
+    try:
+        
+        huayClub_url = config.get('HuayClubSection', 'huayClub_url')
+        huayClub_user = config.get('HuayClubSection', 'huayClub_user')
+        huayClub_pass = config.get('HuayClubSection', 'huayClub_pass')
+        print '---------------'
+        print 'PhantomJs is runing...'
         driver = webdriver.PhantomJS(executable_path = phantomjsPath)
+        print '---------------'
         driver.maximize_window()
+        print 'Now Hitting... '+huayClub_url
         driver.get(huayClub_url)
-        print driver.current_url
-        print '=============================='
-        print '>>> HuayClub <<<<'
-
+    
         elem = driver.find_element_by_name('username')
         elem.send_keys(huayClub_user)
         elem = driver.find_element_by_name('password')
@@ -45,63 +50,24 @@ def huayClub():
         elem = driver.find_element_by_xpath('//*[@id="game-type-list"]/div[2]/button')
         elem.click()
 
+        agent = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="by-member-table-2"]/tbody/tr/td[1]'))).text    
         sumAgent =  WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="by-member-table-2"]/tbody/tr/td[11]'))).text
         sumCom = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="by-member-table-2"]/tbody/tr/td[15]'))).text
-
+        
+        driver.close()
+        print'Sum Agent = ' ,agent
         print'Sum Agent = ' ,sumAgent
         print 'Sum Company = ' ,sumCom
 
+        date_object = datetime.date.today()
         wb = load_workbook(dirpath+'\output\Report.xlsx')
-        print '[HuayClub] Opening Excel File..'
+        print '[HuayClub] Opening Excel File...'
         ws = wb['Sheet1']
+        ws['B2'].value=date_object
+        ws['C19'].value=agent
         ws['B19'].value=sumAgent
         ws['H19'].value=sumCom
-        wb.save(dirpath+'\output\Report.xlsx')
-        print '[HuayClub] Save Excel Success!!'
-        print '=============================='
-    except:
-        print "Something Error"
-
-
-def huayClub2():
-    try: 
-        huayClub_url = config.get('HuayClubSection', 'huayClub_url')
-        huayClub_user = config.get('HuayClubSection', 'huayClub_user')
-        huayClub_pass = config.get('HuayClubSection', 'huayClub_pass')
-
-        huayClub_driver = webdriver.Chrome(chromePath)
-        huayClub_driver.maximize_window()
-        huayClub_driver.set_page_load_timeout(10)
         
-        print 'HuayClub'
-    
-        huayClub_driver.get('https://agent.superlot999.com/login')
-        elem = huayClub_driver.find_element_by_name('username')
-        elem.send_keys(huayClub_user)
-        elem = huayClub_driver.find_element_by_name('password')
-        elem.send_keys(huayClub_pass)
-        elem = WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="login-box"]/div/div/form/fieldset/div[2]/button')))
-        elem.click()
-        elem = WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="sidebar-menu"]/li[1]/a')))
-        elem.click()
-        elem = WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="sidebar-menu"]/li[1]/ul/li[2]/a')))
-        elem.click()
-        elem = WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.ID, 'week')))
-        elem.click()
-        elem = huayClub_driver.find_element_by_xpath('//*[@id="game-type-list"]/div[2]/button')
-        elem.click()
-
-        sumAgent =  WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="by-member-table-2"]/tbody/tr/td[11]'))).text
-        sumCom = WebDriverWait(huayClub_driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="by-member-table-2"]/tbody/tr/td[15]'))).text
-        
-        print 'Sum Agent = ' ,sumAgent
-        print 'Sum Company = ' ,sumCom
-
-        wb = load_workbook(dirpath+'\output\Report.xlsx')
-        print '[HuayClub] Opening Excel File..'
-        ws = wb['Sheet1']
-        ws['B19'].value=sumAgent
-        ws['H19'].value=sumCom
         wb.save(dirpath+'\output\Report.xlsx')
         print '[HuayClub] Save Excel Success!!'
         print '=============================='
